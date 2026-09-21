1bce2f3aa1697240941f9a2f0ae55c38d4fb6149
Aris <aris7ide@gmail.com>
2026-09-21 13:09:12 +0200

    test: create test PositionBookIsCorrect()

--- N1.01.Junit/src/test/java/com/service/BookServiceTest.java
+++ N1.01.Junit/src/test/java/com/service/BookServiceTest.java
@@ -20,4 +20,15 @@ class BookServiceTest {
         BookService.getListBooks().put(2,new Book("Tarzan"));
         assertEquals(2,BookService.getListBooks().size());
     }
+
+    @Test
+    void PositionBookIsCorrect() {
+        BookService bookService = new BookService();
+        Book book1 = new Book("Zelda");
+        Book book2 = new Book("Tarzan");
+        BookService.getListBooks().put(1,book1);
+        BookService.getListBooks().put(2,book2);
+        assertEquals("Zelda", BookService.getListBooks().get(1).getName());
+        assertEquals("Tarzan", BookService.getListBooks().get(2).getName());
+    }
 }
